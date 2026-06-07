780158631eadd7bd1b94c8464ebb7d8e7982f8f6
merlinnikolaipl-spec <merlinnikolaipl@gmail.com>
2026-06-07 20:33:28 +0200

    Fix typo in firebase_core import statement

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:firebase_core/firebase_cohre.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'billing/plans.dart';
