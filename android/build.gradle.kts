468dc39347cf822c56a2efe023a51fbf95a9f093
merlinnikolaipl-spec <merlinnikolaipl@gmail.com>
2026-07-10 09:38:51 +0200

    Add Firebase Crashlytics Gradle plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,7 @@ buildscript {
 
         // Google Services (Firebase)
         classpath("com.google.gms:google-services:4.4.2")
+            classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.2")
     }
 }
 
